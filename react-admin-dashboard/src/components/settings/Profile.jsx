c254df302c5bbdbf5d47fc1736b35456a3b18376
SriDevi <uniquesridevi0629@gmail.com>
2025-03-10 19:18:20 +0530

    Removed embedded Git repo from react-admin-dashboard

---
+++ react-admin-dashboard/src/components/settings/Profile.jsx
@@ -0,0 +1,26 @@
+import { User } from "lucide-react";
+import SettingSection from "./SettingSection";
+
+const Profile = () => {
+	return (
+		<SettingSection icon={User} title={"Profile"}>
+			<div className='flex flex-col sm:flex-row items-center mb-6'>
+				<img
+					src='https://i.pinimg.com/736x/a2/cf/06/a2cf06375c0dbba23de5092a8a997cbc.jpg'
+					alt='Profile'
+					className='rounded-full w-20 h-20 object-cover mr-4'
+				/>
+
+				<div>
+					<h3 className='text-lg font-semibold text-gray-100'>Srijit :)</h3>
+					<p className='text-gray-400'>uniquesridevi0629@gmail.com</p>
+				</div>
+			</div>
+
+			<button className='bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto'>
+				Edit Profile
+			</button>
+		</SettingSection>
+	);
+};
+export default Profile;
